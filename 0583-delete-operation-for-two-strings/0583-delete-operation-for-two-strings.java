3d33b8ed7e7258e2ae191d002d330d2de87a9262
Shanthinath Shedabale <102481278+shanthi1710@users.noreply.github.com>
2025-05-04 15:32:15 +0530

    Time: 7 ms (72.49%), Space: 44.9 MB (55.15%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestCommonSubsequence(String t1, String t2) {
+        int n = t1.length();
+        int m = t2.length();
+
+        int dp[][]= new int[n+1][m+1];
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(t1.charAt(i-1)==t2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    public int minDistance(String w1, String w2) {
+        int res = w1.length() + w2.length() - 2*longestCommonSubsequence(w1, w2);
+        return res;
+    }
+}
