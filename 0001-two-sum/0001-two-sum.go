aecc0e0eb2cf5d12fd97c1f8832e804c2265e39d
Shanthinath Shedabale <102481278+shanthi1710@users.noreply.github.com>
2026-08-28 15:59:52 +0530

    Time: 0 ms (100%), Space: 5.8 MB (54.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,10 +1,13 @@
 func twoSum(nums []int, target int) []int {
-        for i:=0;i<len(nums);i++{
-            for j:=i+1;j<len(nums);j++{
-                if nums[i]+nums[j] == target{
-                    return []int{i,j};
-                }
-            }
+    mp := make(map[int]int)
+
+    for i,num := range nums{
+        res  := target - num
+        if index,exists := mp[res]; exists{
+            return []int{index,i}
         }
-    return []int{};
+
+        mp[num]=i
+    }
+    return []int{}
 }
